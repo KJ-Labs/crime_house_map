76f4a9870744e9b1758c4a12da0129aac2152818
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2021-01-23 18:08:33 -0800

    Changing google maps api key format

--- client/src/components/map/MapChart.js
+++ client/src/components/map/MapChart.js
@@ -17,7 +17,7 @@ class SimpleMap extends Component {
       // Important! Always set the container height explicitly. Updating
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: `${process.env.REACT_APP_GOOGLEMAPS}`}}
+          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLEMAPS}}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
