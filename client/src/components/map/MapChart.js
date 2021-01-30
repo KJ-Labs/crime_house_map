29812d29fef479e79f4672ba3f5ae51ca70b8fbf
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2021-01-30 11:31:23 -0800

    Updating to confirm Heroku does not have problems with packages

--- client/src/components/map/MapChart.js
+++ client/src/components/map/MapChart.js
@@ -1,72 +1,60 @@
-import React from 'react';
-import { GoogleApiWrapper, InfoWindow, Map, Marker } from 'google-maps-react';
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 
 
-class SimpleMap extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showingInfoWindow: false,
-      activeMarker: {},
-      selectedPlace: {}
-    }
-    // binding this to event-handler functions
-    this.onMarkerClick = this.onMarkerClick.bind(this);
-    this.onMapClick = this.onMapClick.bind(this);
-  }
-  onMarkerClick = (props, marker, e) => {
+
+export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  };
+
+  onMarkerClick = (props, marker, e) =>
     this.setState({
       selectedPlace: props,
       activeMarker: marker,
       showingInfoWindow: true
     });
-  }
-  onMapClick = (props) => {
+
+  onClose = props => {
     if (this.state.showingInfoWindow) {
       this.setState({
         showingInfoWindow: false,
         activeMarker: null
       });
     }
-  }
-  render() {
+  };
 
+  render() {
     return (
       <Map
-        item
-        xs = { 12 }
-        style={{ height: '100vh', width: '100%' }}
-        google = { this.props.google }
-        onClick = { this.onMapClick }
-        zoom = { 12 }
-        initialCenter = {{ lat: 47.6219, lng: -122.3517 }}
-      >
-        <Marker
-          onClick = { this.onMarkerClick }
-          title = { 'Marker1' }
-          position = {{ lat: 47.6219, lng: -122.3517 }}
-            ></Marker>
-        <Marker
-          onClick = { this.onMarkerClick }
-          title = { 'Marker2' }
-          position = {{ lat: 50.6219, lng: -122.3517 }}
-            ></Marker>
+      google={this.props.google}
+      zoom={14}
+      style={{ height: '100vh', width: '100%' }}
+      initialCenter={{ lat: 47.6205, lng: -122.3493}
+      }
+    >
+      <Marker
+        onClick={this.onMarkerClick}
+        name={'Space Needle'}
+        imgUrl= {'https://aptamigo-production.s3.amazonaws.com/uploads/property/profile_photo/1032/a486a78b-54ca-4961-a403-5ea5c05a10be.jpg'}
 
-<InfoWindow
-          marker={this.state.activeMarker}
-          onClose={this.onInfoWindowClose}
-          visible={this.state.showingInfoWindow}
-        >
-          <div>
-            <h4>{this.state.selectedPlace.title}</h4>
-          </div>
-        </InfoWindow>
-      </Map>
+      />
+      <InfoWindow
+        marker={this.state.activeMarker}
+        visible={this.state.showingInfoWindow}
+        onClose={this.onClose}
+      >
+        <div>
+          <h4>{this.state.selectedPlace.name}</h4>
+        </div>
+      </InfoWindow>
+    </Map>
     );
   }
 }
 
 export default GoogleApiWrapper({
   apiKey: process.env.REACT_APP_GOOGLEMAPS
-})(SimpleMap);
-
+})(MapContainer);
