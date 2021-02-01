81edfbf298670f68535098915ceb72b6287ae0ff
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2021-01-31 17:35:57 -0800

    Updating to have color changing icons based on criminal offense

--- client/src/components/map/MapChart.js
+++ client/src/components/map/MapChart.js
@@ -1,6 +1,7 @@
 
 import React, { Component } from "react";
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+
 import places from './crimedata.js'
 
 class MapView extends Component {
@@ -41,11 +42,14 @@ class MapView extends Component {
         google={this.props.google}
         className={"map"}
         initialCenter={{ lat: 47.6205, lng: -122.3493}}
-        zoom={this.props.zoom}
         style={{ height: '100vh', width: '100%' }}
       >
+
         {places.map((place, i) => {
           return (
+
+
+
             <Marker
               key={i}
               onClick={this.handleMarkerClick}
@@ -54,11 +58,33 @@ class MapView extends Component {
                 lng: parseFloat(place.longitude)
 
               }}
+
+              icon={{
+
+                url: place.offense_parent_group == "ASSAULT OFFENSES" ? "/googlemarkersyellow.svg"
+                : place.offense_parent_group == "BURGLARY/BREAKING&ENTERING" ?"/googlemarkersdarkorange.svg"
+                : place.offense_parent_group == "TRESPASS OF REAL PROPERTY" ?"/googlemarkersorange.svg"
+                : place.offense_parent_group == "STOLEN PROPERTY OFFENSES" ?"/googlemarkersgreen.svg"
+                : place.offense_parent_group == "SEX OFFENSES" ?"/googlemarkersblue.svg"
+                : place.offense_parent_group == "DESTRUCTION/DAMAGE/VANDALISM OF PROPERTY" ?"/googlemarkersdarkgreen.svg"
+                : place.offense_parent_group == "DRUG/NARCOTIC OFFENSES" ?"/googlemarkersdarkgray.svg"
+                : place.offense_parent_group == "ROBBERY" ?"/googlemarkersdarkpurple.svg"
+                : place.offense_parent_group == "MOTOR VEHICLE THEFT " ?"/googlemarkerspink.svg"
+                : place.offense_parent_group == "HOMICIDE OFFENSES" ?"/googlemarkersteal.svg"
+                : place.offense_parent_group == "ARSON" ?"/googlemarkerslightblue.svg"
+                : place.offense_parent_group == "HUMAN TRAFFICKING" ?"/googlemarkersteal.svg"
+                : place.offense_parent_group == "PROSTITUTION OFFENSES" ?"/googlemarkerstan.svg"
+                : place.offense_parent_group == "PEEPING TOM" ?"/googlemarkersblack.svg"
+                : `/googlemarkerdefault.svg`,
+                scaledSize: new window.google.maps.Size(50, 50)
+              }}
               placeIndex={i}
               name={place.offense}
             />
+
           );
         })}
+
         <InfoWindow
           marker={this.state.activeMarker}
           visible={this.state.showingInfoWindow}
@@ -81,7 +107,6 @@ class MapView extends Component {
 export default GoogleApiWrapper({
   apiKey: process.env.REACT_APP_GOOGLEMAPS
 })(MapView);
-
 
 
 
